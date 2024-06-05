128c0e07b9303466ad14b4cec2054083bf704726
Kata Csizmadia <csizmadia.kata@gmail.com>
2024-06-05 13:14:39 +0200

    refactor TownJudge methods

--- src/main/scala/com/rockthejvm/solutions/graphs/FindTownJudge.scala
+++ src/main/scala/com/rockthejvm/solutions/graphs/FindTownJudge.scala
@@ -2,24 +2,37 @@ package com.rockthejvm.solutions.graphs
 
 object FindTownJudge extends App {
 
-  def findJudge(n: Int, lst: List[(Int,Int)]): Int = {
-    val judge = (1 to n).toList.filter(elem =>
-      !lst.exists(_._1 == elem) &&
-        ((1 to n).toSet - elem).forall(num => lst.contains((num, elem)))
-    ) :+ -1
+  /*
+      n people, 1 to n
+      trust = List[(a, b)]
+      (a,b) = a trusts b
+
+      There might be a town judge.
+        The town judge trusts nobody. == outDegree(tj) = 0
+        Everybody (except for the town judge) trusts the town judge. == inDegree(tj) = n-1
+        There is exactly one person that satisfies these properties.
+
+      Find the town judge, or return -1.
+     */
+  def findJudge(pplCount: Int, edges: List[(Int,Int)]): Int = {
+    val judge = (1 to pplCount).toList.filter(curPerson =>
+      !edges.exists { case (truster, trustee) => truster == curPerson } &&
+        ((1 to pplCount).toSet - curPerson).forall(curOtherPerson => edges.contains((curOtherPerson, curPerson)))
+      ) :+ -1
     judge.head
   }
 
-  def indegree(n: Int, lst: List[(Int, Int)]): Int = {
-    lst.foldLeft(0)((curCount, tuple) => if (tuple._2 == n) curCount + 1 else curCount)
-  }
-  def outdegree(n: Int, lst: List[(Int, Int)]): Int = {
-    lst.foldLeft(0)((curCount, tuple) => if (tuple._1 == n) curCount + 1 else curCount)
-  }
-  def findJudheV2(n: Int, lst:List[(Int, Int)]): Int = {
-    val judge = (1 to n).find(elem => indegree(elem, lst) == n - 1 && outdegree(elem, lst) == 0)
-    if (!judge.isEmpty) judge.get
-    else -1
+  def indegreeOf(node: Int, edgeList: List[(Int, Int)]): Int = {
+    edgeList.foldLeft(0) { case (curCount, (curTruster, curTrustee)) => if (curTrustee == node) curCount + 1 else curCount
+    }
   }
 
+  def outdegreeOf(node: Int, edges: List[(Int, Int)]): Int = {
+    edges.foldLeft(0){case (curCount, (curTruster, curTrustee)) => if (curTruster == node) curCount + 1 else curCount}
+  }
+
+  def findJudgeV2(nodeCount: Int, edges:List[(Int, Int)]): Int = {
+    val judge = (1 to nodeCount).find(elem => indegreeOf(elem, edges) == nodeCount - 1 && outdegreeOf(elem, edges) == 0)
+    judge.getOrElse(-1)
+  }
 }
