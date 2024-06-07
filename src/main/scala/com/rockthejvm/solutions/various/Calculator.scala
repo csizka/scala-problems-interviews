fb8d834cdbbef6cd776c3c7716a571d404639df8
Kata Csizmadia <csizmadia.kata@gmail.com>
2024-06-07 12:04:00 +0200

    refactor Calculator fns

--- src/main/scala/com/rockthejvm/solutions/various/Calculator.scala
+++ src/main/scala/com/rockthejvm/solutions/various/Calculator.scala
@@ -4,8 +4,8 @@ import scala.annotation.tailrec
 
 object Calculator extends App {
 
-  def parseString(string: String): Array[String] = {
-    string.split(" ")
+  def parseString(string: String): Vector[String] = {
+    string.split(" ").toVector
   }
   def addOrSubstract(num1: Int, num2: Int, symbol: String): Int =  symbol match {
     case "+" => num1 + num2
@@ -17,27 +17,34 @@ object Calculator extends App {
   }
 
   def evaluate(str: String): Int = {
-    val tocalculate = parseString(str)
-    val allNums = tocalculate.filter (elem => elem.toIntOption.isDefined).map (_.toInt).toList
-    val allSymbols = tocalculate.filter(elem => Set("+", "-", "*", "/").contains(elem)).toList
+    val toCalculate = parseString(str)
+    val allNums = toCalculate.flatMap(elem => elem.toIntOption match {
+      case Some(num) => List(num)
+      case None => None
+    })
+    val allSymbols = toCalculate.flatMap(elem =>
+      if (Set("+", "-", "*", "/").contains(elem)) List(elem)
+      else None)
 
     @tailrec
-    def evaluateRecursive(curNums: List[Int], curSymbols: List[String]): Int = {
+    def evaluateRecursive(curNums: Vector[Int], curSymbols: Vector[String]): Int = {
       if (curNums.length == 1) curNums.head
       else {
         val indexedSymbols = curSymbols.zipWithIndex
         val priorityOperation: Option[(String, Int)] = indexedSymbols.find{ case (elem, ix) => Set("*", "/").contains(elem)}
-        if (priorityOperation.isDefined) {
-          val (symbol, symbolIx) = priorityOperation.get
-          val partialResult = divideOrMultiply(curNums(symbolIx), curNums(symbolIx + 1), symbol)
-          val (nextNumFstPart, nextNumLastPart) = curNums.splitAt(symbolIx + 1)
-          val nextNums = nextNumFstPart.dropRight(1) ++ nextNumLastPart.updated(0, partialResult)
-          val (nextSymbolFstPart, nextSymbolLastPart) = curSymbols.splitAt(symbolIx)
-          val nextSymbols = nextSymbolFstPart ++ nextSymbolLastPart.drop(1)
-          evaluateRecursive(nextNums, nextSymbols)
-        } else {
-          val partialResult = addOrSubstract(curNums.head, curNums(1), curSymbols.head)
-          evaluateRecursive(partialResult :: curNums.drop(2), curSymbols.drop(1))
+        // TODO: match on the Option instead
+        priorityOperation match {
+          case Some((symb, ix)) =>
+            val (symbol, symbolIx) = (symb, ix)
+            val partialResult = divideOrMultiply(curNums(symbolIx), curNums(symbolIx + 1), symbol)
+            val (nextNumFstPart, nextNumLastPart) = curNums.splitAt(symbolIx + 1)
+            val nextNums = nextNumFstPart.dropRight(1) ++ nextNumLastPart.updated(0, partialResult)
+            val (nextSymbolFstPart, nextSymbolLastPart) = curSymbols.splitAt(symbolIx)
+            val nextSymbols = nextSymbolFstPart ++ nextSymbolLastPart.drop(1)
+            evaluateRecursive(nextNums, nextSymbols)
+          case None =>
+            val partialResult = addOrSubstract(curNums.head, curNums(1), curSymbols.head)
+            evaluateRecursive(partialResult +: curNums.drop(2), curSymbols.drop(1))
         }
       }
     }
@@ -48,8 +55,8 @@ object Calculator extends App {
   val testString2 = " 100 + 2 * 3 - 2 * 10 + 6 / 3"
   val testString3 = " 10 / 5 * 4 + 2 * 10 - 1"
 
-  println(evaluate(testString))
-  println(evaluate(testString2))
-  println(evaluate(testString3))
+  assert(evaluate(testString) == 116)
+  assert(evaluate(testString2) == 88)
+  assert(evaluate(testString3) == 27)
 
 }
