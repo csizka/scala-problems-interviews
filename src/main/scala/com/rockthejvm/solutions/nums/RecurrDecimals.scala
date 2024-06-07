8f32264d1e1d608a122c0ccd5ddd750b2c6349f6
Kata Csizmadia <csizmadia.kata@gmail.com>
2024-06-07 16:32:26 +0200

    refactor RecurDecimals fns

--- src/main/scala/com/rockthejvm/solutions/nums/RecurrDecimals.scala
+++ src/main/scala/com/rockthejvm/solutions/nums/RecurrDecimals.scala
@@ -3,32 +3,38 @@ package com.rockthejvm.solutions.nums
 import scala.annotation.tailrec
 
 object RecurrDecimals extends App {
-  def fractionRecurDecimals(numerator: Long, denominator: Long): String = {
+  def fractionToRecurringDecimals(numerator: Long, denominator: Long): String = {
     val quotient0 = numerator / denominator
     val remainder0 = numerator % denominator
     @tailrec
-    def recurIndex(curRem: Long, remList: List[Long], curIx: Int): Option[Int] = {
-      if (remList.isEmpty) None
-      else if (curRem == remList.head) Some(curIx)
-      else recurIndex(curRem, remList.tail, curIx + 1)
+    def recurIndex(curRem: Long, prevRems: List[Long], curIx: Int): Option[Int] = {
+      if (prevRems.isEmpty) None
+      else if (curRem == prevRems.head) Some(curIx)
+      else recurIndex(curRem, prevRems.tail, curIx + 1)
     }
     @tailrec
-    def f2d(num: Long, denom: Long, quot: List[Long], rem: List[Long]): String = {
+    def f2d(num: Long, denom: Long, previousQuots: List[Long], prevRemains: List[Long]): String = {
       val quotient = num * 10 / denom
       val remaining = num * 10 % denom
-      val recurIxOption = recurIndex(remaining, rem, 0)
+      val recurIxOption = recurIndex(remaining, prevRemains, 0)
 
-      if (remaining == 0) (quot :+ quotient).mkString("")
-      else if (recurIxOption.isEmpty) f2d(remaining, denom, quot :+ quotient, rem :+ remaining)
+      if (remaining == 0) (previousQuots :+ quotient).mkString("")
+      else if (recurIxOption.isEmpty) f2d(remaining, denom, previousQuots :+ quotient, prevRemains :+ remaining)
       else {
-        val (beforRecur, recur) = (quot :+ quotient).splitAt(recurIxOption.get)
-        s"${beforRecur.mkString("")}(${recur.mkString("")})"
+        val (beforeRecur, recur) = (previousQuots :+ quotient).splitAt(recurIxOption.get)
+        s"${beforeRecur.mkString("")}(${recur.mkString("")})"
       }
     }
     if (remainder0 == 0) quotient0.toString
     else if (numerator / denominator.toDouble < 0) s"-$quotient0.${f2d(Math.abs(remainder0), Math.abs(denominator), List.empty, List.empty)}"
     else s"$quotient0.${f2d(remainder0, denominator, List.empty, List(remainder0))}"
   }
-  println(fractionRecurDecimals(-1,-333))
+  println(fractionToRecurringDecimals(-1,-333))
   println(1/333.0)
+  println(fractionToRecurringDecimals(1,6))
+  println(1/6.0)
+  println(fractionToRecurringDecimals(1, 1023))
+  println(1 / 1023.0)
+  println(fractionToRecurringDecimals(1, 13))
+  println(1 / 13.0)
 }
