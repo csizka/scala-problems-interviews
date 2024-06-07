e31198cbc1d2dd065bd340db76d451583d54d574
Kata Csizmadia <csizmadia.kata@gmail.com>
2024-06-07 17:05:04 +0200

    refactor ReverseInteger fns

--- src/main/scala/com/rockthejvm/solutions/nums/ReverseInteger.scala
+++ src/main/scala/com/rockthejvm/solutions/nums/ReverseInteger.scala
@@ -4,28 +4,28 @@ import scala.annotation.tailrec
 
 object ReverseInteger extends App {
 
+  // return a number with the digits reversed
+  // if the result overflows Int(in either direction), return 0
   def reverseInt(num: Int): Int = {
     @tailrec
-    def intReverser(n: Int, acc: Int): Int = {
-      if (n == 0) acc
+    def reverseIntHelper(curNum: Int, acc: Int): Int = {
+      if (curNum == 0) acc
       else {
-        val digits = n % 10
+        val digits = curNum % 10
         val tentativeRes = acc * 10 + digits
-        if (tentativeRes >= 0) intReverser(n / 10, tentativeRes)
+        if (tentativeRes >= 0) reverseIntHelper(curNum / 10, tentativeRes)
         else 0
       }
     }
     if (num == Int.MinValue) 0
-    else if (num > 0) intReverser(num, 0)
-    else -intReverser(-num,0)
+    else if (num > 0) reverseIntHelper(num, 0)
+    else -reverseIntHelper(-num,0)
   }
-  println(reverseInt(54388680))
-  println(reverseInt(-1234567))
-  println(reverseInt(Int.MinValue))
-  println(Int.MinValue)
-  println(reverseInt(0))
-  println(reverseInt(-045))
-  println(reverseInt(Int.MaxValue))
-  println(Int.MaxValue)
+  assert(reverseInt(54388680) == 8688345)
+  assert(reverseInt(-1234567) == -7654321)
+  assert(reverseInt(-045) == -54)
+  assert(reverseInt(0) == 0)
+  assert(reverseInt(Int.MinValue) == 0)
+  assert(reverseInt(Int.MaxValue) == 0)
 
 }
