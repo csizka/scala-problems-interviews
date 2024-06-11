21be2b7de0c3dcf0665be126ebd08f6fefb422f5
Kata Csizmadia <csizmadia.kata@gmail.com>
2024-06-11 16:02:20 +0200

    refactor parentheses fns

--- src/main/scala/com/rockthejvm/solutions/strings/Parentheses.scala
+++ src/main/scala/com/rockthejvm/solutions/strings/Parentheses.scala
@@ -1,38 +1,55 @@
 package com.rockthejvm.solutions.strings
 
 import scala.annotation.tailrec
+import scala.collection.immutable.WrappedString
 
 object Parentheses extends App{
 
-  def hasValidPatentheses(str: String): Boolean = {
+  def hasValidParentheses(str: String): Boolean = {
+    val endIx = str.length - 1
+    val parenths = str.toSeq
     @tailrec
-    def validPhHelper(string: String, acc: (Int, Int)): Boolean = {
-      val (opening, closing) = acc
+    def validPhHelper(string: WrappedString, opening: Int, closing: Int, curIx: Int): Boolean = {
       if (closing > opening) false
-      else if (!string.isEmpty && string.head == '(') validPhHelper(string.tail, (opening + 1, closing))
-      else if (!string.isEmpty && string.head == ')') validPhHelper(string.tail, (opening, closing + 1))
-      else if (!string.isEmpty) validPhHelper(string.tail, acc)
-      else opening == closing
+      else {
+        val (counted, toCount) = string.splitAt(curIx)
+        toCount match {
+          case '(' +: restChars => validPhHelper(string, opening + 1, closing, curIx + 1)
+          case ')' +: restChars => validPhHelper(string, opening, closing + 1, curIx + 1)
+          case WrappedString.empty => opening == closing
+          case _ => validPhHelper(string, opening, closing, curIx + 1)
+        }
+      }
     }
-    validPhHelper(str, (0,0))
+    validPhHelper(parenths, 0, 0, 0)
+  }
+  def hasValidParenthesesV2(str: String): Boolean = {
+    val (canBeValid, openingPhCount, closingPhCount) = str.foldLeft((true, 0, 0)) { case ((canBValid, curOpening, curClosing), curPharentheses) =>
+      val nextOpeningCount = if (curPharentheses == '(') curOpening + 1 else curOpening
+      val nextClosingCount = if (curPharentheses == ')') curClosing + 1 else curClosing
+      val nextCanBValid = canBValid && nextClosingCount <= nextOpeningCount
+    if (!nextCanBValid) (false, 0, 0)
+    else (true, nextOpeningCount, nextClosingCount)
+    }
+    canBeValid && openingPhCount == closingPhCount
   }
 
   def genAllValidParenths(n: Int): Set[String] = {
     @tailrec
-    def genHelper(num: Int, acc: Set[String]): Set[String] = {
-      if (num == n) acc
+    def genHelper(parenthsToAdd: Int, curNumOfParenthsOptions: Set[String]): Set[String] = {
+      if (parenthsToAdd == n) curNumOfParenthsOptions
       else {
         val curStrings = for {
-          elem <- acc
+          elem <- curNumOfParenthsOptions
           index <- 0 to elem.length
         } yield {
           val (before,after) = elem.splitAt(index)
           before + "()" + after
         }
-        genHelper(num + 1, curStrings)
+        genHelper(parenthsToAdd + 1, curStrings)
       }
     }
-    if (n <=0) Set{"Pls add a valid num."}
+    if (n <= 0) throw new Exception(s" please add a number, that is begger than zero. You wrote: '$n'")
     genHelper(1, Set("()"))
   }
 
@@ -45,15 +62,26 @@ object Parentheses extends App{
   val nGoodTst3 = "(j sa ()nbs nb))"
   val nGoodTst4 = "(sajh sakc"
   val nGoodTst5 = "()shacc ((jsaj)ksa Cks"
-  assert(hasValidPatentheses(goodTst1))
-  assert(hasValidPatentheses(goodTst2))
-  assert(hasValidPatentheses(goodTst3))
-  assert(hasValidPatentheses(goodTst4))
-  assert(!hasValidPatentheses(nGoodTst1))
-  assert(!hasValidPatentheses(nGoodTst2))
-  assert(!hasValidPatentheses(nGoodTst3))
-  assert(!hasValidPatentheses(nGoodTst4))
-  assert(!hasValidPatentheses(nGoodTst5))
+
+  assert(hasValidParenthesesV2(goodTst1))
+  assert(hasValidParenthesesV2(goodTst2))
+  assert(hasValidParenthesesV2(goodTst3))
+  assert(hasValidParenthesesV2(goodTst4))
+  assert(!hasValidParenthesesV2(nGoodTst1))
+  assert(!hasValidParenthesesV2(nGoodTst2))
+  assert(!hasValidParenthesesV2(nGoodTst3))
+  assert(!hasValidParenthesesV2(nGoodTst4))
+  assert(!hasValidParenthesesV2(nGoodTst5))
+  assert(hasValidParenthesesV2(goodTst1))
+  assert(hasValidParentheses(goodTst2))
+  assert(hasValidParentheses(goodTst3))
+  assert(hasValidParentheses(goodTst4))
+  assert(!hasValidParentheses(nGoodTst1))
+  assert(!hasValidParentheses(nGoodTst2))
+  assert(!hasValidParentheses(nGoodTst3))
+  assert(!hasValidParentheses(nGoodTst4))
+  assert(!hasValidParentheses(nGoodTst5))
+
 
   assert(genAllValidParenths(1) == Set("()"))
   assert(genAllValidParenths(2) == Set("(())", "()()"))
