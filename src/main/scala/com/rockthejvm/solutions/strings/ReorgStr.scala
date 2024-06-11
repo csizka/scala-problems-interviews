1a8f93f17a5fc984f5f72134a634cfe09bcaa2bb
Kata Csizmadia <csizmadia.kata@gmail.com>
2024-06-11 16:46:29 +0200

    refactor reorgStr fn

--- src/main/scala/com/rockthejvm/solutions/strings/ReorgStr.scala
+++ src/main/scala/com/rockthejvm/solutions/strings/ReorgStr.scala
@@ -4,23 +4,24 @@ import scala.annotation.tailrec
 
 object ReorgStr {
 
-  def reorgStr(str: String): String = {
+  // rearrange chars so that no two adjacent chars are identical
+  def reorgStr(ogString: String): Either[String,List[Char]] = {
     @tailrec
-    def reorg(map: Map[Char, Int], acc: String = "", forbiddenChar: Char = '\u0000'): String = {
-      if (map.isEmpty) acc
+    def reorg(restCharCounts: Map[Char, Int], curRes: List[Char], forbiddenChar: Char = '\u0000'): List[Char] = {
+      if (restCharCounts.isEmpty) curRes
       else {
-        val (curChar, curCount) = (map - forbiddenChar).maxBy{ case (x, y) => y}
-        val newMap =
-          if (curCount > 1) map + (curChar -> (curCount - 1))
-          else map - curChar
-        reorg(newMap, curChar + acc)
+        val (curChar, curCount) = (restCharCounts - forbiddenChar).maxBy{ case (x, y) => y}
+        val newCharCount =
+          if (curCount > 1) restCharCounts + (curChar -> (curCount - 1))
+          else restCharCounts - curChar
+        reorg(newCharCount, curChar :: curRes, curChar)
       }
-      }
-    val length = str.length
-    val mappedStr = str.groupBy(identity).view.mapValues(_.length).toMap
+    }
+    val length = ogString.length
+    val mappedStr = ogString.groupBy(identity).view.mapValues(_.length).toMap
     val possible = !mappedStr.values.exists(_ > (length + 1) / 2)
-    if (!possible) "Too many repetitions, not possibleto reorg. :("
-    else reorg(mappedStr)
+    if (!possible) Left(s"Too many repetitions in the input:'$ogString', not possible to reorg. :(")
+    else Right(reorg(mappedStr, List()))
   }
 
   def main(args: Array[String]): Unit = {
@@ -30,10 +31,10 @@ object ReorgStr {
     val testsize = 6
     assert(testMap == Map('a' -> 6))
     assert(testMap.exists(x => x._2 > testsize / 2 + 1))
-    println(reorgStr("aaab"))
-    println(reorgStr("aaaa"))
-    println(reorgStr("aabb"))
-    println(reorgStr("aaabc"))
+    assert(reorgStr("aaab") == Left("Too many repetitions in the input:'aaab', not possible to reorg. :("))
+    assert(reorgStr("aaaa") == Left("Too many repetitions in the input:'aaaa', not possible to reorg. :("))
+    assert(reorgStr("aabb") == Right(List('a','b','a','b')) || reorgStr("aabb") ==  Right(List('b','a','b', 'a')))
+    assert(reorgStr("aaabc") == Right(List('a','b','a','c','a')) || reorgStr("aaabc") == Right(List('a','c','a','b','a')))
     println(reorgStr("515155323"))
     println(reorgStr("nsjlzzzzzaazz"))
 
