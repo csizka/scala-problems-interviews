16c616b9b23311b33426230ff5a36ce51c673ec4
Kata Csizmadia <csizmadia.kata@gmail.com>
2024-06-20 22:54:44 +0200

    add exercises for abstract stuff

--- src/main/scala/com/rockthejvm/solutions/various/flatmap/advanced/AbstractStuff.scala
+++ src/main/scala/com/rockthejvm/solutions/various/flatmap/advanced/AbstractStuff.scala
@@ -3,41 +3,97 @@ package com.rockthejvm.solutions.various.flatmap.advanced
 object AbstractStuff {
   // if both are Some, return a tuple of the results, otherwise None
   // try using flatMap or for (you can use match for deconstructing the tuple though)
-  def tupledOption[T, U](tuple: (Option[T], Option[U])): Option[(T, U)] = ???
+
+  def tupledOption[T, U](tuple: (Option[T], Option[U])): Option[(T, U)] = {
+    val (lhs, rhs) = tuple
+    lhs.flatMap(val1 => rhs.flatMap(val2 => Some(val1, val2)))
+  }
+
+  def tupledOptionFor[T, U](tuple: (Option[T], Option[U])): Option[(T, U)] = {
+    val (lhs, rhs) = tuple
+    for {
+      val1 <- lhs
+      val2 <- rhs
+    } yield (val1, val2)
+  }
 
   // NOTE: the error types need to be the same
   // if both are Right, return the tuple of the results, otherwise the first error
   // try using flatMap or for (you can use match for deconstructing the tuple though)
-  def tupledEither[L, R1, R2](tuple: (Either[L, R1], Either[L, R2])): Either[L, (R1, R2)] = ???
+
+  def tupledEither[L, R1, R2](tuple: (Either[L, R1], Either[L, R2])): Either[L, (R1, R2)] = {
+    val (lhs, rhs) = tuple
+    lhs.flatMap(val1 => rhs.flatMap(val2 => Right(val1, val2)))
+  }
+
+  def tupledEitherFor[L, R1, R2](tuple: (Either[L, R1], Either[L, R2])): Either[L, (R1, R2)] = {
+    val (lhs, rhs) = tuple
+    for {
+      val1 <- lhs
+      val2 <- rhs
+    } yield (val1, val2)
+  }
+
+  def tupledEitherFlatMap[L, R1, R2](tuple: (Either[L, R1], Either[L, R2])): Either[L, (R1, R2)] =  {
+    val (lhs, rhs) = tuple
+    lhs.flatMap(val1 => rhs.flatMap(val2 => Right(val1, val2)))
+  }
+
 
   // QUESTION (for tupled stuff): if you used flatMap, do you notice any similarities?
-
   // similar to tupled, but for a vector of options. if any fails, the whole thing fails
-  // HINT:                                                                                                                                                                                                                try using recursion / foldLeft       +       you'll also need to match on the input vector, and flatMap on the first element         + if running into Option vs Some issues, use the Option constructor instead of Some
-  def sequenceOption[T](xs: Vector[Option[T]]): Option[Vector[T]] = ???
+
+  def sequenceOption[T](xs: Vector[Option[T]]): Option[Vector[T]] = {
+    xs.foldLeft(Some(Vector()): Option[Vector[T]]) { case (curState, curElem) =>
+      curState.flatMap( vector => curElem.map(value => vector :+ value))
+    }
+  }
+
+  def sequenceOptionFor[T](xs: Vector[Option[T]]): Option[Vector[T]] = {
+    xs.foldLeft(Some(Vector()): Option[Vector[T]]) { case (curState, curElem) =>
+      for {
+        vector <- curState
+        elem <- curElem
+      } yield vector :+ elem
+    }
+  }
 
   // same as tupledEither, but for a vector of eithers. if any fails, the whole thing fails
   // note that the final failure should be the very first one
-  // HINT:                                                                                                                                                                                                                try using recursion / foldLeft       +       you'll also need to match on the input vector, and flatMap on the first element         + if running into Either vs Right issues, add a type annotation to the value constructed with Right liek so 'Right(5) : Either[String, Int]'
-  def sequenceEither[L, R](xs: Vector[Either[L, R]]): Either[L, Vector[R]] = ???
-
+ def sequenceEither[L, R](xs: Vector[Either[L, R]]): Either[L, Vector[R]] = {
+    xs.foldLeft(Right(Vector()): Either[L, Vector[R]]){ case (curState, curElem) =>
+      curState.flatMap( vector => curElem.map(value => vector :+ value))
+    }
+  }
 
   // QUESTION (for sequence stuff): do you notice any similarities?
+  // similar to sequenceOption, but it gets a function as input
+  def traverseOption[T, U](xs: Vector[T], f: T => Option[U]): Option[Vector[U]] = {
+    xs.foldLeft(Some(Vector()): Option[Vector[U]]) { case (curState, curElem) =>
+      curState.flatMap( vector => f(curElem).map(value => vector :+ value))
+    }
+  }
 
+  def traverseOptionWMap[T, U](xs: Vector[T], f: T => Option[U]): Option[Vector[U]] = {
+    sequenceOption(xs.map(f))
+  }
 
   // similar to sequenceOption, but it gets a function as input
-  // HINT:                                                                                                                                                                                                               try using map + sequence
-  def traverseOption[T, U](xs: Vector[T], f: T => Option[U]): Option[Vector[U]] = ???
 
-  // similar to sequenceOption, but it gets a function as input
-  // HINT:                                                                                                                                                                                                               try using map + sequence
-  def traverseEither[L, R1, R2](xs: Vector[R1], f: R1 => Either[L, R2]): Either[L, Vector[R2]] = ???
+  def traverseEither[L, R1, R2](xs: Vector[R1], f: R1 => Either[L, R2]): Either[L, Vector[R2]] = {
+    xs.foldLeft(Right(Vector()): Either[L, Vector[R2]] ) { case (curState, curElem ) =>
+      curState.flatMap( vector => f(curElem).map(value => vector :+ value))
+    }
+  }
 
+  def traverseEitherWMap[L, R1, R2](xs: Vector[R1], f: R1 => Either[L, R2]): Either[L, Vector[R2]] = {
+    sequenceEither(xs.map(f))
+  }
 
   // QUESTION (for traverse stuff): do you notice any similarities?
 
   // as you can see tupled, sequence and traverse are basically both for Option and Either
-  // in fact, this the implementation is the same for anything tha supports flatMap + map + pure
+  // in fact, this the implementation is the same for anything that supports flatMap + map + pure
   // (where we wtore Option(...) or Right(...) we could have written pure)
   // and since map can be expressed with flatMap + pure, we only really need those two,
   // and we can automatically define tupled, sequence and traverse for the type
@@ -52,7 +108,7 @@ object AbstractStuffTests extends App {
   assert(tupledOption(None, Some(5)) == None)
   assert(tupledOption(None, None) == None)
 
-  // NOTE: usually the compputation in the tuple are not so simple, for example:
+  // NOTE: usually the computations in the tuple are not so simple, for example:
   assert(
     tupledOption(
       getFilms("John", 2000),
@@ -71,15 +127,23 @@ object AbstractStuffTests extends App {
 
 
   assert(tupledEither(Right(5), Right("asd")) == Right((5, "asd")))
+  assert(tupledEither(Right(5), Right("asd")) == tupledEitherFor(Right(5), Right("asd")))
+  assert(tupledEither(Right(5), Right("asd")) == tupledEitherFlatMap(Right(5), Right("asd")))
   assert(tupledEither(Left("err1"), Right("asd")) == Left("err1"))
+  assert(tupledEither(Left("err1"), Right("asd")) == tupledEitherFor(Left("err1"), Right("asd")))
+  assert(tupledEither(Left("err1"), Right("asd")) == tupledEitherFlatMap(Left("err1"), Right("asd")))
   assert(tupledEither(Right(5), Left("err2")) == Left("err2"))
   assert(tupledEither(Left("err1"), Left("err2")) == Left("err1"))
 
 
   assert(sequenceOption(Vector()) == Some(Vector()))
+  assert(sequenceOption(Vector()) == sequenceOptionFor(Vector()))
   assert(sequenceOption(Vector(Some(1), Some(2), Some(3))) == Some(Vector(1,2,3)))
+  assert(sequenceOption(Vector(Some(1), Some(2), Some(3))) == sequenceOptionFor(Vector(Some(1), Some(2), Some(3))))
   assert(sequenceOption(Vector(Some(1), Some(2), None)) == None)
+  assert(sequenceOption(Vector(Some(1), Some(2), None)) == sequenceOptionFor(Vector(Some(1), Some(2), None)))
   assert(sequenceOption(Vector(Some(1), None, Some(3))) == None)
+  assert(sequenceOption(Vector(Some(1), None, Some(3))) == sequenceOptionFor(Vector(Some(1), None, Some(3))))
   assert(sequenceOption(Vector(None, Some(2), Some(3))) == None)
   assert(sequenceOption(Vector(None, None, None)) == None)
 
@@ -99,10 +163,10 @@ object AbstractStuffTests extends App {
       )
     )
   )
-  // NOTE (for the above): there's a bit of repetition here.
-  // we are basically calling the same function with different inputs
-  // can we maybe use a function instead ... ?
-  // SPOILER: yes, see traverseOption
+//   NOTE (for the above): there's a bit of repetition here.
+//   we are basically calling the same function with different inputs
+//   can we maybe use a function instead ... ?
+//   SPOILER: yes, see traverseOption
 
 
   assert(sequenceEither(Vector()) == Right(Vector()))
